289c3cb1494307d15430938a5d6e4f407663539c
Prasanth <mvgprasanth@gmail.com>
2024-06-23 16:08:18 -0700

    Additional Files for aie

---
+++ Single_Engine/int8/compile.sh
@@ -0,0 +1,54 @@
+# Vitis compilations
+# list=(
+# 'aie_int8_32x32x32'
+# 'aie_int8_64x64x64'
+
+#)
+
+#list=(
+# 'aie_int8_32x32x32'
+# 'aie_int8_64x64x64'
+# 'aie_int8_16x16x16'
+# 'aie_int8_64x32x64'
+# 'aie_int8_32x64x32'
+# 'aie_int8_64x64x32'
+# 'aie_int8_32x64x64'
+# 'aie_int8_32x16x32'
+# 'aie_int8_16x32x16'
+# 'aie_int8_32x32x16'
+# 'aie_int8_16x32x32'
+# 'aie_int8_128x16x128'
+# 'aie_int8_16x128x16'
+# 'aie_int8_128x128x16'
+# 'aie_int8_16x128x128'
+# 'aie_int8_64x16x64'
+# 'aie_int8_16x64x16'
+# 'aie_int8_64x64x16'
+# 'aie_int8_16x64x64'
+# 'aie_int8_128x8x128'
+# 'aie_int8_8x128x8'
+# 'aie_int8_128x128x8'
+# 'aie_int8_8x128x128'
+# 'aie_int8_64x8x64'
+# 'aie_int8_8x64x8'
+# 'aie_int8_64x64x8'
+# 'aie_int8_8x64x64'
+# 'aie_int8_32x8x32'
+# 'aie_int8_8x32x8'
+# 'aie_int8_32x32x8'
+# 'aie_int8_8x32x32'
+# 'aie_int8_128x128x128'
+# 'aie_int8_16x64x16'
+# )
+
+list=(
+'aie_int8_128x128x128'
+
+)
+
+for i in "${list[@]}"
+do
+    echo "Running " "$i"
+    rm -rf libadf.a
+    make aie aie_kernel_config=$i &> aiecompile_$i.log
+done
